2362ac2012329313c9dbdcbfdba32576725b4109
Jieyi <pokkbaby@gmail.com>
2025-02-10 01:47:42 +0900

    feat(db): add postgresql database connection
    introduce a function to create and return a postgresql
    database connection using psycopg2. update the context teardown to close
    the database connection if it exists. remove explicit closing of redis
    client, as redis-py manages connection pools.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,3 +1,6 @@
+import os
+
+import psycopg2
 import redis
 from flask import Flask, g
 from flask_cors import CORS
@@ -5,6 +8,14 @@ from flask_cors import CORS
 from .api.v1.endpoints import bp as api_bp
 from .config import Config
 from .main.routes import bp as main_bp
+
+
+def get_db_connection():
+    """
+    Create and return a PostgreSQL database connection.
+    :return: connection object
+    """
+    return psycopg2.connect(os.getenv('DATABASE_URL'))  # Get Database URL from environment
 
 
 def create_app():
@@ -18,11 +29,16 @@ def create_app():
     @app.before_request
     def before_request():
         g.redis_client = redis.from_url(app.config['REDIS_URL'])
+        g.db_conn = get_db_connection()  # Initialize database connection
 
     @app.teardown_appcontext
     def teardown_redis(exception):
         if hasattr(g, 'redis_client'):
-            g.redis_client.close()
+            del g.redis_client  # Not necessary to close, as redis-py handles connection pools
+
+        # Close PostgreSQL database connection
+        if hasattr(g, 'db_conn'):
+            g.db_conn.close()
 
     # Register Blueprints
     app.register_blueprint(main_bp)
